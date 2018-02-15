018c5297952892287406c19f3d1adbad551f0934
Brayan Jimenez <jimenezcardenasb@yahoo.com>
2018-02-15 00:27:28 -0500

    slider buapps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
-  clas = false;
-  click(){
-    this.clas = true;
+export class AppComponent implements  OnInit {
+
+  constructor() { }
+
+  ngOnInit(){
+
+    window.addEventListener('scroll', (event) => {
+        console.log(event);
+    });
   }
 }
